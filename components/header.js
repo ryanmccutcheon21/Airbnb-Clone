27707487525cc6c7a13a3ac49496e762c27c80f1
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2022-07-10 18:01:23 -0500

    Setting up state in search bar

--- components/header.js
+++ components/header.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Image from 'next/image'
 import {
     SearchIcon,
@@ -6,8 +7,10 @@ import {
     UserCircleIcon,
     UsersIcon
 } from '@heroicons/react/solid'
+import { useState } from 'react'
 
 function Header() {
+    const [searchInput, setSearchInput] = useState("");
     return (
         <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
             {/* Left */}
@@ -23,6 +26,8 @@ function Header() {
             <div
                 className='flex items-center border-2 rounded-full py-2 md:border-2 md:shadow-sm'>
                 <input
+                    value={searchInput}
+                    onChange={(e) => setSearchInput(e.target.value)}
                     type='text'
                     placeholder='Start your search'
                     className='pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400'
